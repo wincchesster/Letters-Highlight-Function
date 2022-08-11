0d3f37adf53ca199dc853772978d150274d59b0a
Alexander Chaikovsky <wincchesster@gmail.com>
2022-08-11 23:31:21 +0300

    unput active key added

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.addEventListener('keydown', function (event) {
     let keyBoard = document.querySelectorAll('.btn');
     keyBoard.forEach(element => {
-        if (event.code === element.dataset.title) {
+        if (event.code === element.dataset.title) {  
             keyBoard.forEach(key => {
+                // + unput active key
+                if (event.code !== key.dataset.title) 
                 key.classList.remove('active')
             });
             element.classList.toggle('active');
         }
     });
-})
+});
